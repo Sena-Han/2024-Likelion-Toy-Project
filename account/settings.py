408caa68266a1366193026703ec61ca763f6dd03
sen <5.22nhz@gmail.com>
2024-08-08 19:17:04 +0900

    Refactor: todolist 코드 분리

--- account/settings.py
+++ account/settings.py
@@ -135,4 +135,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'signup/static'),
     os.path.join(BASE_DIR, 'guestbook/static'),
+    os.path.join(BASE_DIR, 'todolist/static'),
 ]
